bdbf480efa376c70d60d741ed1a630ae17a9891a
Arseni123321 <coolcoolarseniycool@gmail.com>
2023-01-08 19:54:58 +0300

    js

---
+++ index.js
@@ -0,0 +1,83 @@
+// let a
+// a=5
+// console.log(a);
+
+// const b=5
+// console.log(b);
+// console.log(12345678);
+// console.error(121547);
+// alert(a)
+
+//  const userName = 'Kira'
+// console.log ( `Hello, ${userName} ` )
+
+// console.log ( 'strfh thb "fbgb fvg" trgh etgb' )
+
+// const userName = 'Kira'
+// console.log ( `Hello, ${5+8+8*-865} ` )
+
+// let isGrater = 4<1
+// alert(isGrater);
+
+// let a=null
+// console.log(a);
+// a =5
+// console.log(a);
+
+// const bigInt=12375633574448762345356n;
+
+// const pupil = {
+//     class:'11 "A"',
+//     sex:'man',
+//     age: 16,
+//     name: {
+//         firstName:'Kira',
+//         secondName:'Bortnik'
+//     }
+// }
+// console.log(pupil);
+
+// pupil.class=12
+// console.log(pupil);
+// pupil.interest ='reading'
+// console.log(pupil);
+// delete pupil.sex
+// console.log(pupil);
+
+// const a =-8
+// const b = 10
+// console.log(a+b);
+
+
+// const a =8
+// const b = 'cdfbh '
+// const c = null
+// const d = undefined
+// const e = 10n
+// const f = true
+// console.log(typeof f);
+
+// const num=5
+// console.log(typeof (num +'hello'));
+// console.log(typeof num +'hello');
+// console.log(typeof String (num));
+// console.log(typeof `${num}`);
+// console.log(typeof num);
+
+// const a=1
+// console.log(1+1);
+
+// const a=prompt()
+// console.log(a+1);
+
+// const str='10a'
+// console.log(typeof str);
+// const str1=Number (str)
+// console.log(str1);
+// console.log(typeof Number(str));
+// console.log(typeof +str);
+// console.log(parseInt (str));
+
+const bool=3564
+console.log(typeof Boolean(bool));
+console.log(typeof );
