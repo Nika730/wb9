1433a840030a733f3f5fbfe1ef2327c8730f5b3c
anna <annalymareva3@icloud.com>
2023-02-05 19:53:50 +0300

    asdfg

--- index.js
+++ index.js
@@ -1,129 +1,99 @@
-// const teacher={
-//     name: 'Kira',
-//     age : 15,
-//     sex : 'woman',
-//     job : 'student'
-// }
-// console.log(teacher);
-// const objKeys = Object.keys(obj)
-// console.log(objKeys);
-
-// const objValues = Object.values(obj)
-// console.log(objValues);
-
-// console.log(obj.isMarried);
-// obj.isMarried=true
-// console.log(obj);
-
-// const obj2 = teacher
-// console.log(obj2);
-// // obj2.field = 'qwerty'
-// console.log(teacher);
-// console.log(obj2);
-
-// const obj3={}
-
-// for (  prop in teacher) {
-//     console.log(prop);
-//     obj3[prop] = teacher[prop]
-// }
-// console.log();
-// obj3.lol = 'lol'
-// console.log(teacher);
-// console.log(obj3);
-
-// const obj4= Object.assign(teacher)
-// // obj4.lol = 'lol'
-// console.log('obj4', obj4);
-// const obj5 = Object.freeze(teacher)
-// obj5.lol = 'lol'
-// console.log('obj5',obj5);
-
-// console.log(teacher.hasOwnProperty('sex'));
-
-
-// function sayHello() {
-// console.log('hello!');
-// }
-// sayHello()
-
-// function summ(a,b ) {
-//     console.log(a+b);
-    
-// }
-// summ(4,56)
-// summ(45,546)
-
-// function summ (a,b,c){
-//     console.log(a+b-c);
-// }
-// summ(12,43,3)
-// summ(3,12,43)
-// summ(43,3,12)
-
-// const summ = function (a,b,c) {
-//     console.log(a+b-c);
-// }
-// summ(12,43,3)
-// summ(3,12,43)
-// summ(43,3,12)
-
-// const summ = (a,b,c) => console.log(a+b-c);
-// summ(12,43,3)
-// summ(3,12,43)
-// summ(43,3,12)
-
-// window.onload =(function()) {
-//     console.log('hello');
-// } ()
-
-// function args(a,b,c){
-//     const sum = a+b+c
-// // console.log(sum);
-// // console.log(arguments);
-// }
-// args(1,2,3,4,5,6,7,8,9,)
-
-// function args(a,b,c, ...rest){
-//     const sum = a+b+c
-// console.log(rest);
-// }
-// args(1,2,3,4,5,6,7,8,9,)
-
-// const arr =[ 5,1,2,4,42,2]
-// console.log(...arr);
-
-// function qwe (a,b,c){
-//     const disk = (b*b-4*a*c)
-//     // console.log(disk);
-//     if (disk > 0) {
-//         const x1 = (-b+Math.sqrt(disk))/(2*a)
-//         const x2 = (-b-Math.sqrt(disk))/(2*a)
-//         console.log(x1);
-//         console.log(x2);
-//     }  else if (disk == 0 ) {
-//         const x = (-b-Math.sqrt(disk))/(2*a)
-//         console.log(x);
-//     } else {
-    
-//         console.log('Нет корней');
-//     }
-// }   
-// qwe(-4,28,-49) 
-// qwe(-6,0,54)
-// qwe(1,-1,0)
-// qwe(1,0,-49)
-// qwe(3,-4,94)
-
-// function qwe(str){
-// let counter = 0
-// const arr = [' ','.',',', '!' ]
-   
-//     for (let i = 0; i < str.length; i++ ){
-//         if (str[i] === str[i].toUpperCase()&& !arr.includes(str[i])){
-//             counter++
+// const obj={
+//     firstName: 'kira',
+//     secondName: 'bortnik',
+//     info: function(phone,email ){
+//         console.log(`${this.firstName} ${this.secondName}`);
+//         if (phone && email ){ 
+//         console.log('Phone:', phone);
+//         console.log('Email:', email);
 //         }
 //     }
-//     console.log(counter);
 // }
-// qwe('I Love YoU')
+// obj.info()
+
+// const o1 = {
+//     firstName:'mike',
+//     secondName: 'wazovski',
+// }
+// obj.info.bind(o1)('+375297094699', 'mnika730@gmail.com')
+
+// const o2 = {
+//     firstName:'peter',
+//     secondName: 'pen',
+// }
+// obj.info.call(o2, '+375445332006', 'nmika370@gmail.com')
+
+// const o3 = {
+//     firstName:'doctor',
+//     secondName: 'who',
+// }
+// obj.info.apply(o3, ['+375298070329', 'mammy@gmail.ru'])
+
+
+// const promise = new Promise( (resolve, reject) => {
+//     // setTimeout( () => resolve('done!'), 2000)
+//     setTimeout( () => reject('error!'), 2000)
+// })
+// console.log(promise);
+//     promise.then(
+//         result => console.log(result) //,
+//         // error => console.log(error)
+//  )
+//  .catch(error => console.log(error))
+// .finally( () => console.log('finally'))
+
+
+
+// const url = 'https://jsonplaceholder.typicode.com/users/1'
+
+// const f = async() => {
+//     await fetch(url)
+//     .then(Response => Response.json())
+//     .then(json => console.log(json))
+    
+// }
+ 
+// const a = async () => {
+//     console.log('start');
+//     await f()
+//     console.log('end');
+// }
+// // a()
+
+// const err = () => {
+//     throw 'some error =('
+// }
+
+// const tryCatch = async () => {
+//     try {
+//         await a ()
+//         // err()
+//     } catch (error) {
+//         console.log(error);
+//     } finally {
+//         console.log('finally');
+//     }
+// }
+
+// tryCatch()
+
+
+// console.log('start');
+// setTimeout(() => console.log('setTimeout'), 0)
+// console.log('end');
+
+
+const usersUrl = 'https://jsonplaceholder.typicode.com/users'
+async function getUsers() {
+    let arr;
+   await fetch (usersUrl)
+    .then(response => response.json())
+    // .then(users => console.log(users))
+    .then(users => arr = users)
+    console.log(arr);
+    return arr
+}
+getUsers()
+
+const usresArray = getUsers()
+console.log(usresArray);
