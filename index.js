eefda54ff641be4f3d7fb59bf8b48d2fab25a13c
anna <annalymareva3@icloud.com>
2023-02-12 19:54:37 +0300

    paper.rock

--- index.js
+++ index.js
@@ -1,99 +1,41 @@
-// const obj={
-//     firstName: 'kira',
-//     secondName: 'bortnik',
-//     info: function(phone,email ){
-//         console.log(`${this.firstName} ${this.secondName}`);
-//         if (phone && email ){ 
-//         console.log('Phone:', phone);
-//         console.log('Email:', email);
-//         }
-//     }
+// const btn1=document.getElementById('btn1')
+// console.log(btn1);
+// const btn2_1= document.querySelector('.btn2')
+// console.log(btn2_1);
+// const btn2_2= document.querySelector('#btn2')
+// console.log(btn2_2);
+
+// const li = document.querySelectorAll('li')
+// console.log(li);
+// for (let elem of li ) {
+//     console.log(elem);
+//     console.log(elem.matches('li[value="6"]'));
 // }
-// obj.info()
 
-// const o1 = {
-//     firstName:'mike',
-//     secondName: 'wazovski',
-// }
-// obj.info.bind(o1)('+375297094699', 'mnika730@gmail.com')
+// const chapter = document.querySelector ('.chapter')
+// console.log(chapter.closest('.book')); //ul
+// console.log(chapter.closest('.contents')); //div
+// console.log(chapter.closest('h1')); //null (потому что h1 не родитель)
 
-// const o2 = {
-//     firstName:'peter',
-//     secondName: 'pen',
-// }
-// obj.info.call(o2, '+375445332006', 'nmika370@gmail.com')
-
-// const o3 = {
-//     firstName:'doctor',
-//     secondName: 'who',
-// }
-// obj.info.apply(o3, ['+375298070329', 'mammy@gmail.ru'])
-
-
-// const promise = new Promise( (resolve, reject) => {
-//     // setTimeout( () => resolve('done!'), 2000)
-//     setTimeout( () => reject('error!'), 2000)
+// const inpWrite = document.querySelector('#writting')
+// inpWrite.addEventListener('change', () => {
+//     console.log(inpWrite.value);
+//     p.innerHTML = 'hello'
 // })
-// console.log(promise);
-//     promise.then(
-//         result => console.log(result) //,
-//         // error => console.log(error)
-//  )
-//  .catch(error => console.log(error))
-// .finally( () => console.log('finally'))
+// inpWrite.addEventListener('input', () => {
+//     console.log(inpWrite.value);
+//     p.innerHTML = inpWrite.value
+// })
 
-
-
-// const url = 'https://jsonplaceholder.typicode.com/users/1'
-
-// const f = async() => {
-//     await fetch(url)
-//     .then(Response => Response.json())
-//     .then(json => console.log(json))
-    
+// const inpWrite = document.querySelector('#writting')
+// const p = document.querySelector('#p')
+// const remove = document.querySelector('#btn-rem')
+// const inputFun = () => {
+//     console.log(inpWrite.value);
+//     p.innerHTML = inpWrite.value
 // }
- 
-// const a = async () => {
-//     console.log('start');
-//     await f()
-//     console.log('end');
+// inpWrite.addEventListener('input', inputFun)
+// remove.onclick = () => {
+//     alert('remove listener from input')
+//     inpWrite.removeEventListener('input', inputFun)
 // }
-// // a()
-
-// const err = () => {
-//     throw 'some error =('
-// }
-
-// const tryCatch = async () => {
-//     try {
-//         await a ()
-//         // err()
-//     } catch (error) {
-//         console.log(error);
-//     } finally {
-//         console.log('finally');
-//     }
-// }
-
-// tryCatch()
-
-
-// console.log('start');
-// setTimeout(() => console.log('setTimeout'), 0)
-// console.log('end');
-
-
-const usersUrl = 'https://jsonplaceholder.typicode.com/users'
-async function getUsers() {
-    let arr;
-   await fetch (usersUrl)
-    .then(response => response.json())
-    // .then(users => console.log(users))
-    .then(users => arr = users)
-    console.log(arr);
-    return arr
-}
-getUsers()
-
-const usresArray = getUsers()
-console.log(usresArray);
